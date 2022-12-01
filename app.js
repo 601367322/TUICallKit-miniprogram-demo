22b3430f92e05f6a9f117aa27814817fd72cb5a0
Sen <601367322@qq.com>
2022-12-01 13:25:47 +0800

    udpate

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ App({
 
   globalData: {
     config: {
-      userID: '111', //User ID
-      SECRETKEY: 'a00bb702118c491d8f6a7f315d47c767d7d6d4b46cc70e928dc7db4d1434e751', // Your secretKey
-      SDKAPPID: 1400486080, // Your SDKAppID
+      userID: xxx, //User ID
+      SECRETKEY: xxx, // Your secretKey
+      SDKAPPID: xxx, // Your SDKAppID
       EXPIRETIME: 604800,
     },
   },
